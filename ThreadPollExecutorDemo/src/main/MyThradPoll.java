3b6cd71473a7871f259b0d7e6ef4c5616ac279e3
周国栋 <zhouguodong@zhouguodongdeMacBook-Pro.local>
2019-10-22 23:47:42 +0800

    add ThreadPollExecutor

--- ThreadPollExecutorDemo/src/main/MyThradPoll.java
+++ ThreadPollExecutorDemo/src/main/MyThradPoll.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MyThradPoll {
     public static void main(String[] args) {
-        ThreadPoolExecutor threadPoolExecutor=new ç(2,5,5l, TimeUnit.SECONDS, new LinkedBlockingQueue<>(5));
+        ThreadPoolExecutor threadPoolExecutor=new ThreadPoolExecutor(2,5,5l, TimeUnit.SECONDS, new LinkedBlockingQueue<>(5));
         //threadPoolExecutor.execute();
     }
 }
